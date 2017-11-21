1adfa9117dda016deb36463e164a5ba2d6787b17
lastgenRichtoffen <christoffer.thorsson.te15d@gmail.com>
2017-11-21 10:17:32 +0100

    added getters
    Added the code for getting the Strings firstname and lastname

--- src/person.java
+++ src/person.java
@@ -2,7 +2,15 @@
 public class person {
 
 	private String firstname;
-	private String Lastname;
+	private String lastname;
 	
+	public String getFirstname() {
+		return firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	
+
 	
 }
