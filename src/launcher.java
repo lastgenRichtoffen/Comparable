fba69219422b35a1f3e79974ce0e7de0d61727e8
lastgenRichtoffen <christoffer.thorsson.te15d@gmail.com>
2017-11-22 13:38:54 +0100

    added launcher
    Created a class, launcher and added a main-method for it. Launcher scanns the text-file "names.txt", creates an arraylist called people and creates a person for each line within the textfile, which it adds to the arraylist list.

---
+++ src/launcher.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class launcher {
+
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner sc = new Scanner (new File("names.txt"));
+		ArrayList<person> list = new ArrayList<person>();
+		while(sc.hasNextLine()){
+			person p = new person(sc.nextLine().trim());
+			list.add(p);
+		}
+	}
+	
+}
